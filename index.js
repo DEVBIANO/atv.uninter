e1eb0296429033a7101c952a9a685b58c0319ac0
DEVBIANO <altamirfabiano2007@gmail.com>
2025-05-04 10:05:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const Meubotao = document.getElementById('Meubotao');
+
+Meubotao.addEventListener('click',function(){
+
+    const mensagem = document.getElementById('OBRIGADO POR VER')
+    mensagem.textContent ='PARABENS POR ESTAR AQUI';
+
+    mensagem.style.display = 'block';
+});
